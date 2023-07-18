45d2e2d450ea4b12321bc4bf901c49358d498186
Jamie Hong <jamiehong@berkeley.edu>
2023-07-17 19:32:57 -0700

    bash, zsh: remove conda, extra paths, etc.

--- bashfig.sh
+++ bashfig.sh
@@ -18,24 +18,8 @@ export PATH=$EXISTING_PATH
 
 # PATH
 
-# pandoc
-export PATH="/opt/homebrew/bin/pandoc:${PATH}"
-
-# GNU make
-export PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
-
-# python
-export PATH="/Library/Developer/CommandLineTools/usr/bin/python3:${PATH}"
-
 # rust
 export PATH="$HOME/.cargo/bin:$PATH"
-
-# jupyterlab
-export PATH="$HOME/.local/bin:$PATH"
-
-# Set path for mvim to work on the command line
-# For launching MacVim from the terminal
-export PATH="/Applications/MacVim.app/Contents/bin:${PATH}"
 
 # For homebrew: brew
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -45,21 +29,3 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 if [ -f /opt/homebrew/etc/profile.d/bash_completion.sh ]; then
     . /opt/homebrew/etc/profile.d/bash_completion.sh
 fi
-
-# Installed miniconda via homebrew. The resulting block is from running:
-# conda init "$(basename "${SHELL}")"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
